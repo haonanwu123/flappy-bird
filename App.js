2c95a693b7831edfd3f3ce74c8ced36a04a4342d
haonanwu123 <wuhaonan99@gmail.com>
2024-04-22 10:28:10 +0200

    assets download

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Canvas, useImage, Image } from "@shopify/react-native-skia";
+import { useWindowDimensions } from "react-native";
 
-export default function App() {
+const App = () => {
+  const { width, height } = useWindowDimensions();
+  const bg = useImage(require("./assets/sprites/background-day.png"));
+  const bird = useImage(require("./assets/sprites/yellowbird-upflap.png"));
+  const pipe = useImage(require("./assets/sprites/pipe-green.png"));
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Canvas style={{ width, height }}>
+      <Image image={bg} width={width} height={height} fit={"cover"}></Image>
+      <Image image={pipe} y={height - 320} x={width / 2} width={103} height={640}></Image>
+      <Image image={pipe} y={-320} x={width / 2} width={103} height={640}></Image>
+      <Image
+        image={bird}
+        height={64}
+        width={48}
+        y={height / 2}
+        x={width / 4}
+      ></Image>
+    </Canvas>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # flappy-build
+
+## expo install
+ start the development server
+ - npx expo
+
+ see a list of commands in expo cli
+
+ - npx expo --help
+
+ ## react native skia install
+- npx expo install @shopify/react-native-skia
+
+## start app
+- cd flappy-build
+
+run on android systeem
+- npx expo start -android
+
+run on IOS systeem
+- npx expo start -ios
+
+run on web
+- npx expo start -web
